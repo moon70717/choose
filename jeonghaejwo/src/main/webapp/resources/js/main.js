8b642b23f019c662d00fb0252844ab4619ef37cb
DJA <DJA@DJA-PC>
2018-03-09 19:09:53 +0900

    asdf

--- jeonghaejwo/src/main/webapp/resources/js/main.js
+++ jeonghaejwo/src/main/webapp/resources/js/main.js
@@ -12,7 +12,7 @@ Path.prototype.setHeight = function(height) {
   this.segments[1].point.y = this.segments[0].point.y + height;
   return this.segments[2].point.y = this.segments[3].point.y + height;
 };
-
+ 
 Path.prototype.reset = function() {
   this.setWidth(0);
   this.setHeight(0);
