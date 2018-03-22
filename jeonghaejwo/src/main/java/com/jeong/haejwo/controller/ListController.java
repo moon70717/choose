8b6e5141e43f9d8e3c5149e09fb875d490ac67dd
DJA <DJA@DJA-PC>
2018-03-22 18:43:16 +0900

    123

--- jeonghaejwo/src/main/java/com/jeong/haejwo/controller/ListController.java
+++ jeonghaejwo/src/main/java/com/jeong/haejwo/controller/ListController.java
@@ -1,19 +1,42 @@
 package com.jeong.haejwo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/list")
 public class ListController {
-
-	@RequestMapping("/list")
-	public @ResponseBody Map<String,Object> getList(@RequestParam()Map<String,Object> data){
+//@RequestParam()Map<String,Object> data
+	@RequestMapping("/all")
+	public @ResponseBody List<Object> getList(){
+		Map<String,Object> result=new HashMap<String,Object>();
+		List<Object> in=new ArrayList<Object>();
+		result.put("userNo", "1");
+		result.put("code", "1");
+		result.put("point", 1);
+		result.put("comment", "1");
+		result.put("date", "1998-08-03");
+		result.put("contenttypeid", "1");
+		result.put("placename", "1");
+		result.put("imgNo", null);
+		in.add(result);
 		
-		return null;
+		result=new HashMap<String,Object>();
+		result.put("userNo", "2");
+		result.put("code", "2");
+		result.put("point", 2);
+		result.put("comment", "2");
+		result.put("date", "1998-08-03");
+		result.put("contenttypeid", "2");
+		result.put("placename", "2");
+		result.put("imgNo", null);
+		in.add(result);
+		return in;
 	}
 }
