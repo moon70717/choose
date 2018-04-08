e8b464267b6b03e4ab07dd5ccff1713b38432db6
moon7 <moon7@DESKTOP-935OSE5>
2018-04-08 22:42:16 +0900

    s3 되나 테스트

--- jeonghaejwo/src/main/java/com/jeong/haejwo/controller/FileController.java
+++ jeonghaejwo/src/main/java/com/jeong/haejwo/controller/FileController.java
@@ -1,6 +1,5 @@
 package com.jeong.haejwo.controller;
 
-import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.jeong.haejwo.util.UploadObject;
 
 @Controller
 @RequestMapping("/file")
@@ -35,8 +37,11 @@ public class FileController {
 			// aws 업로드시 주소변경필요
 			// 또는 관리자 페이지를 만들어서 수정 가능하도록 변경
 			// aws 업로드시 /media/회원코드/년월일.jpg 경로를 이렇게 변경
-			File f = new File("\\media\\" + file.getOriginalFilename());
-			file.transferTo(f);
+			ObjectMetadata metadata = new ObjectMetadata();
+			UploadObject uploadObject = new UploadObject();
+			uploadObject.upload(file.getOriginalFilename(), file.getInputStream(),metadata);
+			/*File f = new File("\\media\\" + file.getOriginalFilename());
+			file.transferTo(f);*/
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
